ea4ad3fc11ca68934477e759b400fecea8175666
nadeemm06 <72907518+nadeemm06@users.noreply.github.com>
2020-10-20 11:19:26 +0530

    Update Calc.java

--- Calc.java
+++ Calc.java
@@ -2,10 +2,15 @@ class Calc{
 public static int add(int a,int b){
     return a+b;
 }
-public static int add(int a,int b){
+public static int sub(int a,int b){
     return a-b;
+}
+public static int prod(int a,int b){
+    return a*b;
 }
 public static void main(String []args){
      System.out.println(add(10,20));
+    System.out.println(add(50,40));
+    System.out.println(add(20,20));
 }
 }
